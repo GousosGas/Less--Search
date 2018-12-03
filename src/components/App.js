8cce7ba1124bb70cf79fe1413e8638db764070ef
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-12-03 18:44:29 +0200

    main video section created , list of videos created

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React,{Component} from 'react';
 import SearchBar from './SearchBar';
 import Youtube from '../apis/Youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends Component{
 
     state={
-        videos:[]
+        videos:[],
+        selectedVideo:null
     };
 
     onTermSubmit=async term=>{
@@ -21,11 +23,19 @@ class App extends Component{
         this.setState({videos:response.data.items})
     };
 
+    onVideoSelect =(video)=>{
+      this.setState({selectedVideo:video});
+
+    };
+
     render(){
         return <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit}  />
             <p>Found {this.state.videos.length} videos</p>
-            <VideoList videos={this.state.videos}/>
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}/>
         </div>
     }
 }
